d5f953c4129755ad16ca026fae3b68ecad63ac7a
Gullit Moran <gullitmoran@hotmail.com>
2023-08-03 18:57:22 -0500

    configuracion de variables de enterno

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # cinemapedia
 
-A new Flutter project.
+# Dev
+
+1. Copiar el .env.template y renombralo a .env
+2. Cambiar las variables de enterno (The MoviesBD)
---
+++ lib/config/constants/environment.dart
@@ -0,0 +1,10 @@
+
+
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Environment {
+
+  static String theMovieDBKey = dotenv.env['THE_MOVIEDB_KEY'] ?? 'No hay api key';
+
+}
