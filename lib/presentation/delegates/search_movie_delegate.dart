ebeafcb4aedac0c469eab08059bc12732f0ea937
Gullit Moran <gullitmoran@hotmail.com>
2023-09-05 17:05:48 -0500

    Search peliculas

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,120 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?>{
+
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies
+  });
+
+  @override
+  String get searchFieldLabel => 'Buscar película';
+
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+
+      //if(query.isNotEmpty)
+        FadeIn(
+          animate: query.isNotEmpty,
+          duration: const Duration(milliseconds: 200),
+          child: IconButton(
+            onPressed: () => query = '', 
+            icon: const Icon(Icons.clear)
+          ),
+        )
+
+
+
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null), 
+      icon: const Icon(Icons.arrow_back_ios_new_outlined)
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text('buildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            return _MovieItem(movie: movies[index]);
+          },
+        );
+      }, 
+    );
+  }
+
+}
+
+class _MovieItem extends StatelessWidget {
+
+  final Movie movie;
+
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width * 0.20,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(movie.posterPath),
+            ),
+          ),
+
+          const SizedBox(width: 10),
+
+          SizedBox(
+            width: size.width * 0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(movie.title, style: textStyles.titleMedium,),
+                (movie.overview.length > 100) 
+                ? Text('${movie.overview.substring(0, 100)}...')
+                : Text(movie.overview),
+
+                
+              ],
+            ),
+          )
+
+
+        ],
+      ), 
+    );
+  }
+}
+
+
