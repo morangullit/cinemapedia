689d760cef0631d932abd23436744927461bf2cd
Gullit Moran <gullitmoran@hotmail.com>
2023-09-09 11:14:44 -0500

    Isar Database Store Local

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 1. Copiar el .env.template y renombralo a .env
 2. Cambiar las variables de enterno (The MoviesBD)
+3. Cambios en la entidad, hay que ejecutar el comando
+
+´´´
+flutter pub run build_runner build
+´´´
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -212,6 +212,13 @@ class _CustomSliverAppBar extends StatelessWidget {
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
       shadowColor: Colors.red,
+      actions: [
+        IconButton(
+          onPressed: (){},
+          icon: Icon(Icons.favorite_border),
+          //icon: const Icon(Icons.favorite_rounded, color: Colors.red,),
+        )
+      ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         // title: Text(
@@ -231,39 +238,68 @@ class _CustomSliverAppBar extends StatelessWidget {
                 },
               ),
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
+
+            const _CustomGradient(
+              begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [0.0, 0.2],
+                    colors: [
+                      Colors.black54,
+                      Colors.transparent
+                    ]),
+            
+            const _CustomGradient(
+              begin: Alignment.topCenter,
                     end: Alignment.bottomCenter,
-                    stops: [0.7, 1.0],
+                    stops: [0.8, 1.0],
                     colors: [
                       Colors.transparent,
-                      Colors.black87
+                      Colors.black54
                     ]
-                  )
-                )
-              ),
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
+            const _CustomGradient(
+              begin: Alignment.topLeft,
                     stops: [0.0, 0.3],
                     colors: [
                       Colors.black87,
                       Colors.transparent,
-                    ]
-                  )
-                )
-              ),
-            ),
+                    ])
           ],
         ),
       ),
 
     );
+  }
+}
+
+
+class _CustomGradient extends StatelessWidget {
+
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+
+  const _CustomGradient({
+    this.begin = Alignment.centerLeft, 
+    this.end =Alignment.centerRight, 
+    required this.stops, 
+    required this.colors
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: begin,
+                    end: end,
+                    stops: stops,
+                    colors: colors,
+                  )
+                )
+              ),
+            );
   }
 }
